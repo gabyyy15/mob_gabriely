87a7ce600692be57dfcb5a6e38387925ed61a2ad
gbyy <cabral.gabriely@escola.pr.gov.br>
2026-09-22 21:10:38 +0000

    exercicio 4 ok

--- app-laboratorio/App.js
+++ app-laboratorio/App.js
@@ -5,6 +5,26 @@ const eu = {
   idade: 17,
   cidade: "cascavel"
 };
+const eu2 = {
+  nome: "João",
+  nota1: 8,
+  nota2: 6,
+};
+
+function calcularMedia(objeto) {
+  return (objeto.nota1 + objeto.nota2) / 2;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Média: {calcularMedia(eu)}</h1>
+    </div>
+  );
+}
+
+export default App;
+
 
 export default function App() {
   return (
